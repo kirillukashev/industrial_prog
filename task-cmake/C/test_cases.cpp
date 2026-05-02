231489a53b662c6c61737e98dff3fa7250b6ae75
kirill <kirill>
2026-05-02 11:58:01 +0300

    fix

--- task-cmake/C/test_cases.cpp
+++ task-cmake/C/test_cases.cpp
@@ -2,11 +2,8 @@
 #include "A/index.h"
 #include "B/lib.h"
 
-TEST(IndexHeaderTest, ProjectNameDefined) {
+TEST(IndexHeaderTest, HeaderIsCorrect) {
     EXPECT_STREQ(PROJECT_NAME, "IndustrialProg");
-}
-
-TEST(IndexHeaderTest, MagicNumbersCorrect) {
     EXPECT_EQ(MAGIC_COUNT, 10);
     EXPECT_EQ(MAGIC_NUMBERS[0], 1);
     EXPECT_EQ(MAGIC_NUMBERS[1], 1);
@@ -14,13 +11,10 @@ TEST(IndexHeaderTest, MagicNumbersCorrect) {
     EXPECT_EQ(MAGIC_NUMBERS[9], 55);
 }
 
-TEST(LibTest, AddFunction) {
+TEST(LibTest, LibIsCorrect) {
     EXPECT_EQ(add(2, 3), 5);
     EXPECT_EQ(add(-1, 1), 0);
     EXPECT_EQ(add(0, 0), 0);
-}
-
-TEST(LibTest, FibonacciFunction) {
     EXPECT_EQ(fibonacci(0), 0);
     EXPECT_EQ(fibonacci(1), 1);
     EXPECT_EQ(fibonacci(10), 55);
